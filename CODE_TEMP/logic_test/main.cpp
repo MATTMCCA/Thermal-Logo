34a336dce2845bf4efc46649e7442e938b96a38b
MATTMCCA <mccardlematthew0@gmail.com>
2021-12-31 17:50:14 -0500

    removed logic
    replaced logic with bitmap & offset value

--- CODE_TEMP/logic_test/main.cpp
+++ CODE_TEMP/logic_test/main.cpp
@@ -7,11 +7,12 @@
 #define COOL 0
 #define HEAT 1
 
-const int cell     = 10; /* 10 cells per pix */
-const int ms_delay = 10; /* 10ms per E */
+#define E2_OFFSET 5                         /* tune wire offset */
+
+const int micro_pixle = 20;                 /* 20 upix per pix */
 
 int THERM_CNT[3]   = {      0,    0,    0};
-int THERM[3]       = {   HEAT, HEAT, HEAT};
+int     THERM[3]   = {   HEAT, HEAT, HEAT};
 
 int SEQ[3][3]      = {
   // E1,   E2,   E3
@@ -20,38 +21,41 @@ int SEQ[3][3]      = {
   {COLD, COLD, COLD}  //state 3, E3
 };
 
+/* logo, replaces logic */
+const int  bmp_size  = 10;
+int bmp[3][bmp_size] = {
+    {0, 1, 1, 1, 1, 0, 1, 1, 1, 1},
+    {0, 1, 0, 0, 0, 1, 0, 0, 0, 1},
+    {0, 1, 0, 1, 0, 1, 0, 1, 0, 1}
+};
+
 int u_thold = 6; //heat limit, start to cool
-int l_thold = 5;  //cool, start to heat
-int cnt     = 0;
-int i       = 0;
+int l_thold = 5; //cool, start to heat
+int cnt = 0, i = 0, e = 0, e2 = 0;
 
 void state_seq(void);
 
 int main()
 {
     printf("LOGO-TEST!\n");
+    int upix_cnt = micro_pixle * bmp_size;
+    int E1[upix_cnt] = {0x00};
+    int E2[upix_cnt] = {0x00};
+    int E3[upix_cnt] = {0x00};
 
-    int cell_cnt = cell * 19;
-    int E1[cell_cnt] = {0x00};
-    int E2[cell_cnt] = {0x00};
-    int E3[cell_cnt] = {0x00};
-
-    while(cnt < cell_cnt)
-    {
+    while(cnt < upix_cnt) {
       state_seq();
-      for (int i = 0; i < 3; i++) {
+
+      for (i = 0; i < 3; i++) {
         if (SEQ[i][i] == COLD) {
           THERM_CNT[i] = 0;
           THERM[i] = HEAT;
+        } else {
+          if ((THERM_CNT[i] >= u_thold) && (THERM[i] == HEAT)) THERM[i] = COOL;
+          if ((THERM_CNT[i] <= l_thold) && (THERM[i] == COOL)) THERM[i] = HEAT;
+          if (THERM[i] == HEAT)                                THERM_CNT[i]++;
+          else                                                 THERM_CNT[i]--;
         }
-      }
-
-      for (i = 0; i < 3; i++) {
-        if ((THERM_CNT[i] >= u_thold) && (THERM[i] == HEAT)) THERM[i] = COOL;
-        if ((THERM_CNT[i] <= l_thold) && (THERM[i] == COOL)) THERM[i] = HEAT;
-
-        if (THERM[i] == HEAT) THERM_CNT[i]++;
-        else                  THERM_CNT[i]--;
       }
 
       E1[cnt] = SEQ[0][0] & THERM[0];
@@ -60,15 +64,15 @@ int main()
       cnt++;
     }
 
-    //display
+    //display for visual inspection
     int *g[3] = {(int*)&E1, (int*)&E2, (int*)&E3};
     for(int j = 0; j < 3; j++) {
-        int l = cell;
+        int l = micro_pixle/2;
         if(j == 2 ) l*=3;
         for(int b = 0; b < l; b++) {
             printf("E%d: ", j+1);
             int *h = g[j];
-            for(int u = 0; u < cell_cnt; u++) {
+            for(int u = 0; u < upix_cnt; u++) {
                 if(h[u] == 1) printf("#");
                 else          printf(".");
             }
@@ -80,26 +84,17 @@ int main()
 
 /* WORK LOGO LOGIC */
 void state_seq(void) {
+  e = ((cnt)              / micro_pixle) % bmp_size; //mod prevent out of bounds indexing
+  e2 = ((cnt + E2_OFFSET) / micro_pixle) % bmp_size; //mod prevent out of bounds indexing
+  SEQ[0][0] = SEQ[1][1] = SEQ[2][2] = COLD;
   /* E1 *************************************************/
-  if (((cnt > (1  * cell)) && (cnt < (9  * cell))) ||
-      ((cnt > (11 * cell)) && (cnt < (19 * cell))))
-    SEQ[0][0] = HOT;
-  else
-    SEQ[0][0] = COLD;
+  if(bmp[0][e] == 1)  SEQ[0][0] = HOT;
   /* E2 *************************************************/
-  if (((cnt > (1  * cell)) && (cnt < (3  * cell)))  || //shifted for test
-      ((cnt > (9  * cell)) && (cnt < (11 * cell)))  || //shifted for test
-      ((cnt > (17 * cell)) && (cnt < (19 * cell))))    //shifted for test
-    SEQ[1][1] = HOT;
-  else
-    SEQ[1][1] = COLD;
+  if(bmp[1][e2] == 1) SEQ[1][1] = HOT;
   /* E3 *************************************************/
-  if (((cnt > (1  * cell)) && (cnt < (3  * cell)))  ||
-      ((cnt > (5  * cell)) && (cnt < (7  * cell)))  ||
-      ((cnt > (9  * cell)) && (cnt < (11 * cell)))  ||
-      ((cnt > (13 * cell)) && (cnt < (15 * cell)))  ||
-      ((cnt > (17 * cell)) && (cnt < (19 * cell))))
-    SEQ[2][2] = HOT;
-  else
-    SEQ[2][2] = COLD;
+  if(bmp[2][e] == 1)  SEQ[2][2] = HOT;
 }
+
+
+
+//100
--- CODE_TEMP/sketch_dec18a/sketch_dec18a.ino
+++ CODE_TEMP/sketch_dec18a/sketch_dec18a.ino
@@ -1,11 +1,6 @@
 /*
-   Thermal Logo Code for Board REV_2
+   Thermal Logo Code for Board REV_2 (Adafruit Itsy Bitsy 3v 8Mhz)
    MJM - 2021
-
-   Board = Adafruit Itsy Bitsy 3v 8Mhz
-
-   NOTE:
-     Logic may need work, idk about the thermal control.
 */
 
 #define HOT    1
@@ -15,8 +10,26 @@
 #define COOL 0
 #define HEAT 1
 
-const int cell     = 10; /* 10 cells per pix */
-const int ms_delay = 10; /* 10ms per E */
+#define E2_OFFSET 5                         /* tune wire offset */
+
+const int micro_pixle = 20;                 /* 20 upix per pix */
+const int ms_delay    = 10;                 /* 10ms per E */
+const int u_thold     = 10;                 /* heat limit, start to cool */
+const int l_thold     = 5;                  /* cool, start to heat */
+
+/* logo, replaces logic */
+const int bmp_size  = 10;
+const int bmp[3][bmp_size] = {
+  {0, 1, 1, 1, 1, 0, 1, 1, 1, 1},
+  {0, 1, 0, 0, 0, 1, 0, 0, 0, 1},
+  {0, 1, 0, 1, 0, 1, 0, 1, 0, 1}
+};
+
+/* IO deff */
+const int TRIGGER = 10;
+const int EN_1    = 6;
+const int EN_2    = 8;
+const int EN_3    = 9;
 
 int THERM_CNT[3]   = {      0,    0,    0};
 int THERM[3]       = {   HEAT, HEAT, HEAT};
@@ -28,17 +41,7 @@ int SEQ[3][3]      = {
   {COLD, COLD, COLD}  //state 3, E3
 };
 
-int TRIGGER = 10;
-/* !!DOUBLE CHECK ORDER!! */
-int EN_1    = 6;
-int EN_2    = 8;
-int EN_3    = 9;
-
-int u_thold = 10; //heat limit, start to cool
-int l_thold = 5;  //cool, start to heat
-
-int cnt     = 0;
-int i       = 0;
+int cnt = 0, i = 0, e = 0, e2 = 0;
 
 void state_seq(void);
 void button_check(void);
@@ -58,73 +61,51 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  /****************************************************/
   button_check();
   state_seq();
-  /****************************************************/
 
-  for (int i = 0; i < 3; i++)
+  for (i = 0; i < 3; i++) {
     if (SEQ[i][i] == COLD) {
       THERM_CNT[i] = 0;
       THERM[i] = HEAT;
+    } else {
+      if ((THERM_CNT[i] >= u_thold) && (THERM[i] == HEAT)) THERM[i] = COOL;
+      if ((THERM_CNT[i] <= l_thold) && (THERM[i] == COOL)) THERM[i] = HEAT;
+      if (THERM[i] == HEAT)                                THERM_CNT[i]++;
+      else                                                 THERM_CNT[i]--;
     }
-
-  for (i = 0; i < 3; i++) {
-    if ((THERM_CNT[i] >= u_thold) && (THERM[i] == HEAT)) THERM[i] = COOL;
-    if ((THERM_CNT[i] <= l_thold) && (THERM[i] == COOL)) THERM[i] = HEAT;
-
-    if (THERM[i] == HEAT) THERM_CNT[i]++;
-    else                  THERM_CNT[i]--;
   }
 
-  for (i = 0; i < 3; i++) {    
-    digitalWrite(EN_1, SEQ[i][0] & THERM[0]); //E1
-    digitalWrite(EN_2, SEQ[i][1] & THERM[1]); //E2
-    digitalWrite(EN_3, SEQ[i][2] & THERM[2]); //E3
+  /* may need to add the 4th state back */
+  for (i = 0; i < 3; i++) {
+    digitalWrite(EN_1, SEQ[i][0] & THERM[0]); /* E1 */
+    digitalWrite(EN_2, SEQ[i][1] & THERM[1]); /* E2 */
+    digitalWrite(EN_3, SEQ[i][2] & THERM[2]); /* E3 */
     delay(ms_delay);
   }
-  
-  cnt++;  
+  cnt++;
 }
 
 void button_check(void) {
-  /* loop while !active */  
+  /* loop while !active */
   while (digitalRead(TRIGGER) == !ACTIVE) {
     digitalWrite(EN_3, COLD);
     digitalWrite(EN_2, COLD);
     digitalWrite(EN_1, COLD);
-    THERM_CNT[0] = THERM_CNT[1] = THERM_CNT[2] = 0;
-        THERM[0] =     THERM[1] =     THERM[2] = HEAT;
-    cnt = 0;
-    delay(100);
+    cnt = THERM_CNT[0] = THERM_CNT[1] = THERM_CNT[2] = 0;
+    THERM[0] = THERM[1] = THERM[2] = HEAT;
+    delay(100); //poll button every ~100ms
   }
 }
 
-/* WORK LOGO LOGIC */
 void state_seq(void) {
+  e  = ((cnt)             / micro_pixle) % bmp_size; //mod prevent out of bounds indexing
+  e2 = ((cnt + E2_OFFSET) / micro_pixle) % bmp_size; //mod prevent out of bounds indexing
+  SEQ[0][0] = SEQ[1][1] = SEQ[2][2] = COLD;
   /* E1 *************************************************/
-  if (((cnt > (1  * cell)) && (cnt < (9  * cell))) ||
-      ((cnt > (11 * cell)) && (cnt < (19 * cell))))
-    SEQ[0][0] = HOT;
-  else
-    SEQ[0][0] = COLD;
+  if (bmp[0][e]  == 1)  SEQ[0][0] = HOT;
   /* E2 *************************************************/
-  if (((cnt > (0  * cell)) && (cnt < (2  * cell)))  ||
-      ((cnt > (8  * cell)) && (cnt < (10 * cell)))  ||
-      ((cnt > (16 * cell)) && (cnt < (18 * cell))))
-    SEQ[1][1] = HOT;
-  else
-    SEQ[1][1] = COLD;
+  if (bmp[1][e2] == 1)  SEQ[1][1] = HOT;
   /* E3 *************************************************/
-  if (((cnt > (1  * cell)) && (cnt < (3  * cell)))  ||
-      ((cnt > (5  * cell)) && (cnt < (7  * cell)))  ||
-      ((cnt > (9  * cell)) && (cnt < (11 * cell)))  ||
-      ((cnt > (13 * cell)) && (cnt < (15 * cell)))  ||
-      ((cnt > (17 * cell)) && (cnt < (19 * cell))))
-    SEQ[2][2] = HOT;
-  else
-    SEQ[2][2] = COLD;
+  if (bmp[2][e]  == 1)  SEQ[2][2] = HOT;
 }
-
-
-//130
